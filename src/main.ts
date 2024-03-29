49a02bfb8e022ff0bdfc2d90107167ece247c6ff
zkflal <zkflal@gmail.com>
2024-03-29 15:19:45 +0900

    🐛 버그수정

--- src/main.ts
+++ src/main.ts
@@ -22,17 +22,18 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
     credentials: true,
+    allowedHeaders: "*",
   });
 
   app.use(cookieParser());
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  //   res.header("Access-Control-Allow-Credentials", "true");
+  //   next();
+  // });
   // const app = await NestFactory.create(AppModule, {
   //   httpsOptions,
   //   cors: true,
