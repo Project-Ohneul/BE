c6dcf73fafc2dd33fb1950b8c827fc37b3d216e1
zkflal <zkflal@gmail.com>
2024-03-29 15:07:54 +0900

    ⚡️ 이판사판

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,11 @@ async function bootstrap() {
   });
 
   app.use(cookieParser());
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
   // const app = await NestFactory.create(AppModule, {
   //   httpsOptions,
   //   cors: true,
