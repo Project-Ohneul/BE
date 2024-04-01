d425ea801adf38931c7cfd97785c56378d6eb0bd
zkflal <zkflal@gmail.com>
2024-04-01 13:39:29 +0900

    ⚡️ Socket cors 변경

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -14,7 +14,7 @@ import { Response } from "express";
 // WebSocketGateway 데코레이터를 이용하여 WebSocketGateway 클래스를 정의합니다.
 @WebSocketGateway({
   cors: {
-    origin: ["http://localhost:4000"], // CORS 설정: 클라이언트 주소
+    origin: ["https://port-0-ohneul-front-754g42alucwss46.sel5.cloudtype.app"], // CORS 설정: 클라이언트 주소
   },
 })
 export class MyGateway implements OnModuleInit, OnGatewayDisconnect {
